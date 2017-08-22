b134156ad1f8f11e5b37c4831e99bd0a0429bb6d
John Currie <johntcurrie@gmail.com>
2017-08-22 08:50:16 -0700

    Changed format of data returned from apiService

--- angular/controllers/characterController.js
+++ angular/controllers/characterController.js
@@ -1,10 +1,15 @@
 marvelApp.controller('characterController', function(dataFetcher) {
-	this.submitted = false;
-	this.characterName = '';
+	var vm = this;
+	vm.submitted = false;
+	vm.characterName = '';
+	vm.characterData = [];
 
-	this.apiFetcher = function() {
-		
-		dataFetcher.getData(this.characterName);
-		this.characterName = '';
+	vm.apiFetcher = function() {
+		if(vm.characterName.length === 0) {
+			return;
+		}
+		vm.characterData = dataFetcher.getData(vm.characterName);
+		vm.submitted = true;
+		vm.characterName = '';
 	}
 })
--- angular/services/apiService.js
+++ angular/services/apiService.js
@@ -3,10 +3,16 @@ marvelApp.service('dataFetcher', function($http) {
 	this.getData = function(character) {
 		var date = new Date();
 		var hash = md5(date.getTime() + privAPI_KEY + pubAPI_KEY);
-		var url = "http://gateway.marvel.com/v1/public/characters?limit=100&apikey="+pubAPI_KEY;
+		var url = "http://gateway.marvel.com/v1/public/characters?limit=100&name="+character+"&apikey="+pubAPI_KEY;
 		url += "&ts=" + date.getTime() + "&hash=" + hash;
 		$http.get(url).then(function mySuccess(response) {
-			console.log('SUCCESS!', response);
+			console.log('SUCCESS!', response.data.data.results[0]);
+			if(response.data.data.results.length === 0) {
+				return null;
+			}
+			else {
+				return response.data.data.results[0];
+			}
 		}), function myError(response) {
 			console.log('ERROR', response)
 		}
