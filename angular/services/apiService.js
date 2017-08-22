ffa06542c6f07a5d671ab51a2e34002660e7d274
John Currie <johntcurrie@gmail.com>
2017-08-21 18:27:51 -0700

    Added script tag for CryptoJS for MD5 conversion

--- angular/services/apiService.js
+++ angular/services/apiService.js
@@ -1,6 +1,17 @@
 marvelApp.service('dataFetcher', function($http) {
 
 	this.getData = function(character) {
-		console.log(character)
+		var date = new Date();
+		$http({
+			method: "GET",
+			apikey: privAPI_KEY,
+			url: "http://gateway.marvel.com/v1/public/characters",
+			ts: date.getTime(),
+			hash: CryptoJS.MD5(date.getTime + privAPI_KEY + pubAPI_KEY)
+		}).then(function mySuccess(response) {
+			console.log('SUCCESS!', response);
+		}), function myError(response) {
+			console.log('ERROR', response)
+		}
 	}
 })
