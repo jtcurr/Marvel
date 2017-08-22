acc97032312b86ead6f2b7a58bcb568495eda75b
John Currie <johntcurrie@gmail.com>
2017-08-21 20:49:46 -0700

    Changed format of get request to Marvel

--- angular/services/apiService.js
+++ angular/services/apiService.js
@@ -2,15 +2,10 @@ marvelApp.service('dataFetcher', function($http) {
 
 	this.getData = function(character) {
 		var date = new Date();
-		var hash = md5(date.getTime + privAPI_KEY + pubAPI_KEY);
+		var hash = md5(date.getTime() + privAPI_KEY + pubAPI_KEY);
 		var url = "http://gateway.marvel.com/v1/public/characters?limit=100&apikey="+pubAPI_KEY;
-		$http({
-			method: "GET",
-			apikey: pubAPI_KEY,
-			url: "http://gateway.marvel.com/v1/public/characters"
-			// ts: date.getTime(),
-			// hash: md5(date.getTime + privAPI_KEY + pubAPI_KEY)
-		}).then(function mySuccess(response) {
+		url += "&ts=" + date.getTime() + "&hash=" + hash;
+		$http.get(url).then(function mySuccess(response) {
 			console.log('SUCCESS!', response);
 		}), function myError(response) {
 			console.log('ERROR', response)
